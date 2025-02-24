8e96885d17a38cb0347b28a5020dcca68163675c
quarkusio <no-reply@quarkus.io>
2025-02-24 11:15:37 +0000

    Initial commit

---
+++ src/test/java/uja/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package uja;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
