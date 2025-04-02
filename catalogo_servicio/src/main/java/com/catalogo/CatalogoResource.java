179e91fccbf9ab810795a3777971808fec68755b
Ignacio Delgado Lobelle <idl00005@red.ujaen.es>
2025-04-02 18:52:55 +0000

    Correccion de la configucacion de JWT

--- catalogo_servicio/src/main/java/com/catalogo/CatalogoResource.java
+++ catalogo_servicio/src/main/java/com/catalogo/CatalogoResource.java
@@ -5,13 +5,14 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import java.util.List;
 
-@Path("/catalog")
+@Path("/catalogo")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CatalogoResource {
 
     @GET
-    @RolesAllowed("admin") // Solo accesible con un token válido
+    @Path("/login")
+    @RolesAllowed("user") // Solo accesible con un token válido
     public List<String> getProducts() {
         return List.of("Producto 1", "Producto 2", "Producto 3");
     }
