693dc7205ca7b97ec740fb5128030cd3fc3e5680
Ignacio Delgado Lobelle <idl00005@red.ujaen.es>
2025-08-16 18:01:46 +0200

    Primera implementación del frontend, creado el componente Producto

---
+++ ecommerce-frontend/frontend/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import Producto from "./components/Producto.vue";
+</script>
+
+<template>
+  <div class="d-inline-flex">
+    <Producto nombre="teclado"
+              precio=20
+              urlImg="https://content.nationalgeographic.com.es/medio/2025/08/14/conejo-de-cola-de-algodon-papiloma_62cd99e2_250814122026_1280x1129.webp">
+    </Producto>
+    <Producto nombre="teclado"
+              precio=20
+              urlImg="https://content.nationalgeographic.com.es/medio/2025/08/14/conejo-de-cola-de-algodon-papiloma_62cd99e2_250814122026_1280x1129.webp">
+    </Producto>
+    <Producto nombre="teclado"
+              precio=20
+              urlImg="https://content.nationalgeographic.com.es/medio/2025/08/14/conejo-de-cola-de-algodon-papiloma_62cd99e2_250814122026_1280x1129.webp">
+    </Producto>
+    <Producto nombre="teclado"
+              precio=20
+              urlImg="https://content.nationalgeographic.com.es/medio/2025/08/14/conejo-de-cola-de-algodon-papiloma_62cd99e2_250814122026_1280x1129.webp">
+    </Producto>
+    <Producto nombre="teclado"
+              precio=20
+              urlImg="https://content.nationalgeographic.com.es/medio/2025/08/14/conejo-de-cola-de-algodon-papiloma_62cd99e2_250814122026_1280x1129.webp">
+    </Producto>
+    <Producto nombre="teclado"
+              precio=20
+              urlImg="https://content.nationalgeographic.com.es/medio/2025/08/14/conejo-de-cola-de-algodon-papiloma_62cd99e2_250814122026_1280x1129.webp">
+    </Producto>
+    <Producto nombre="teclado"
+              precio=20
+              urlImg="https://content.nationalgeographic.com.es/medio/2025/08/14/conejo-de-cola-de-algodon-papiloma_62cd99e2_250814122026_1280x1129.webp">
+    </Producto>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ ecommerce-frontend/frontend/src/components/Producto.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <img :src="urlImg" alt="" width="250">
+    <h3>
+      {{nombre}}
+    </h3>
+    <p class="price">
+      {{precio}}€
+    </p>
+    <button>
+      <i class="bi bi-cart-plus"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Producto',
+  methods: {},
+  props: {
+    nombre: String,
+    precio: Number,
+    urlImg: String
+  }
+}
+import 'bootstrap-icons/font/bootstrap-icons.css'
+</script>
+
+<style scoped>
+  .card{
+    border: 1px solid rgba(184, 184, 184, 0.5);
+    border-radius: 5px;
+    padding: 5px;
+    width: auto;
+    max-width: 275px;
+    box-shadow: 2px 2px 8px rgba(248, 248, 248, 0.75);
+    margin: 20px;
+  }
+  .price{
+    font-size: 25px;
+  }
+</style>
